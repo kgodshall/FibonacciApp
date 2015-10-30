d9aee33f71e9465c9b27bba1a421e316f6e7b3e0
Kira Godshall <kgodshall@kgodshall-2017.local>
2015-10-30 14:12:07 -0400

    FibonacciApp Final

--- FibonacciApp/ViewController.swift
+++ FibonacciApp/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateFibonacciSequence()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var includesZeroLabel: UILabel!
+    @IBOutlet weak var includesZeroSwitch: UISwitch!
+    @IBOutlet weak var numberOfItemsSlider: UISlider!
+    @IBOutlet weak var numberOfItemsLabel: UILabel!
+    
+    
+    
+    var fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: 2, includesZero: true)
+    
+    @IBAction func updateFibonacciSequence() {
+        
+        if includesZeroSwitch.on {
+            numberOfItemsSlider.maximumValue = 94
+        }else{
+            numberOfItemsSlider.maximumValue = 93
+        }
+        
+        includesZeroLabel.text = includesZeroSwitch.on ? "Yes": "No"
+        
+        numberOfItemsLabel.text = String(UInt64(numberOfItemsSlider.value))
+        
+        
+        fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: UInt64(numberOfItemsSlider.value), includesZero: includesZeroSwitch.on)
+        
+        textView.text = fibonacciSequence.values.description
 
-
+    }
 }
-
